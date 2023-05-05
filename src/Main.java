822439e23dc52b150f9ceb450859e359c5ee32a8
Camilo Canclini <camilocanclini@gmail.com>
2023-05-05 17:22:39 -0300

    Adding Cretion Animal Feature

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import clases.Animal;
+import clases.Cat;
+import clases.Menu;
 import clases.Person;
 
 import java.util.*;
@@ -5,6 +8,7 @@ import java.util.*;
 public class Main {
     public static void main (String[] arg){
         Scanner sc = new Scanner(System.in);
+        ArrayList<Animal> animalList = new ArrayList<Animal>();
         int option = 0;
         do{
             System.out.println("+----- M E N U   G U A R D E R I A -----+");
@@ -17,81 +21,40 @@ public class Main {
             System.out.print("--> ");
             try {
                 option = sc.nextInt();
+
             }catch (InputMismatchException e){
                 System.out.println("Error en el ingreso de datos...");
-                sc.nextLine();
+                sc.nextLine(); // Salto de linea para Scanner
             }
             if (!(option>=0 && option<=6)){
                 System.out.println("Ingrese una opción correcta");
+                continue;
             }
-        }while(!(option>=0 && option<=6));
-                if (option == 6){
-                    System.out.println("Saliendo del Sistema...");
-                }
-                switch (option){
-                    case 1:
-                        System.out.println("Opción Seleccionada: " + option);
-                        System.out.println("REGISTRANDO UN ANIMAL EN LA GUARDERIA");
-                        //======================
-                        int opcionTipoAnimal = 0;
-                        do {
-                            System.out.println("TIPO DE ANIMAL");
-                            System.out.println("1) Perro");
-                            System.out.println("2) Gato");
-                            System.out.println("3) Pescado");
-                            System.out.println("4) Hamster");
-                            System.out.print("--> ");
-                            opcionTipoAnimal = sc.nextInt();
-                            if (opcionTipoAnimal < 1 || opcionTipoAnimal > 4)
-                                System.out.println("Opcion invalida!");
-                        }while (opcionTipoAnimal < 1 || opcionTipoAnimal > 4);
+            if (option == 6){
+                System.out.println("Saliendo del Sistema...");
+                break;
+            }
+            switch (option){
+                case 1:
+                    System.out.println("Opción Seleccionada: " + option);
+                    Menu.optionOne(animalList);
+                    System.out.println(animalList.toString());
+                    break;
+                case 2:
+                    System.out.println("Opción Seleccionada: " + option);
+                    break;
+                case 3:
+                    System.out.println("Opción Seleccionada: " + option);
+                    break;
+                case 4:
+                    System.out.println("Opción Seleccionada: " + option);
+                    break;
+                case 5:
+                    System.out.println("Opción Seleccionada: " + option);
+                    break;
+            }
+        }while(true);
+    }
 
-                        System.out.println("Nombre: "); //:String
-                        System.out.println("Edad: "); //:Integer
-                        System.out.println("Genero: "); //:Gender
-                        System.out.println("Peso: "); //:Double
-                        if (opcionTipoAnimal == 1) {
-                            System.out.println("Raza: ");
-                        }
-                        if (opcionTipoAnimal == 3) {
-                            int opcionTipoAgua = 0;
-                            do {
-                                System.out.println("TIPO DE AGUA PESCADO");
-                                System.out.println("1) Fria");
-                                System.out.println("2) Tropical");
-                                System.out.print("--> ");
-                                opcionTipoAgua = sc.nextInt();
-                                if (opcionTipoAgua < 1 || opcionTipoAgua > 2)
-                                    System.out.println("Opcion invalida!");
-                            }while (opcionTipoAgua < 1 || opcionTipoAgua > 2);
-                        }
-                        break;
-                    case 2:
-                        System.out.println("Opción Seleccionada: " + option);
-                        break;
-                    case 3:
-                        System.out.println("Opción Seleccionada: " + option);
-                        break;
-                    case 4:
-                        System.out.println("Opción Seleccionada: " + option);
-                        break;
-                    case 5:
-                        System.out.println("Opción Seleccionada: " + option);
-                        break;
-                }
-    }
-    public Person crearDueno(){
-        Scanner sc = new Scanner(System.in);
-        int dni;
-        String nombre, direccion;
-        System.out.println("DATOS DEL DEL DUEÑO");
-        System.out.print("DNI: "); //:Integer
-        dni = sc.nextInt();
-        System.out.print("Nombre: "); //:String
-        nombre = sc.nextLine();
-        System.out.print("Direccion: "); //:String
-        direccion = sc.nextLine();
-        return new Person(dni,nombre,direccion);
-    }
 }
 
---
+++ src/clases/Menu.java
@@ -0,0 +1,192 @@
+package clases;
+import enums.Environment;
+import enums.Gender;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Menu {
+
+    public static void optionOne(ArrayList<Animal> animalList){
+        Scanner sc = new Scanner(System.in);
+        int animalType = 0;
+        String name;
+        int age;
+        Gender gender;
+        double weight;
+        Person owner;
+
+        Environment fishEnvironment;
+        String breed;
+
+        Animal newAnimal = null;
+
+        // Owner Creation
+        int dni;
+        String ownerName, address;
+        do {
+            System.out.println("REGISTRANDO UN DATOS DUEÑO");
+            //======================
+            System.out.print("DNI: "); //:Integer
+            try{
+                dni = sc.nextInt();
+                sc.nextLine();
+            }catch (InputMismatchException e){
+                System.out.println("Error: Ingreso de datos Incorrecto!");
+                sc.nextLine();
+                continue;
+            }
+            System.out.print("Nombre: "); //:String
+            ownerName = sc.nextLine();
+            if (ownerName.isBlank() || ownerName.matches("[^a-zA-Z]")) {
+                System.out.println("Error: Formato de Nombre Incorrecto!");
+                continue;
+            }
+            System.out.print("Direccion: "); //:String
+            address = sc.nextLine();
+            if (address.isBlank() || !address.matches("^[a-zA-Z]+\s[0-9]{1,6}$")) {
+                System.out.println("Error: Formato de Direccion Incorrecto!");
+                continue;
+            }
+            owner = new Person(dni, ownerName, address);
+            break;
+        }while (true);
+        do {
+            System.out.println("REGISTRANDO UN ANIMAL EN LA GUARDERIA");
+            //======================
+            System.out.println("TIPO DE ANIMAL");
+            System.out.println("1) Perro");
+            System.out.println("2) Gato");
+            System.out.println("3) Pescado");
+            System.out.println("4) Hamster");
+            System.out.print("--> ");
+            try{
+                animalType = sc.nextInt();
+                sc.nextLine();
+            }
+            catch (InputMismatchException e){
+                System.out.println("Opcion invalida!");
+                sc.nextLine();
+                continue;
+            }
+            if (animalType >= 1 && animalType <= 4){
+                break;
+            }else{
+                System.out.println("Opcion invalida!");
+            }
+        }while (true);
+
+        // Loop for Setting The Animal Data (Name, Age, Gender, ...)
+        do {
+            //Animal Name Input
+            System.out.print("Nombre: "); //:String
+            name = sc.nextLine();
+            if (name.isBlank() || !name.matches("^[a-zA-Z]+$")) {
+                System.out.println("Error: Formato de Nombre Incorrecto!");
+                continue;
+            }
+            // Animal Age Input
+            System.out.print("Edad: "); //:Integer
+            try {
+                age = sc.nextInt();
+                sc.nextLine();
+            }catch (InputMismatchException e){
+                System.out.println("Error: Ingreso de datos Incorrecto!");
+                sc.nextLine();
+                continue;
+            }
+            // Animal Gender Input
+            System.out.println("Genero: "); //:Gender
+            System.out.println("Seleccione el Genero: ");
+            System.out.println("1) Macho "); //:Gender
+            System.out.println("2) Hembra: "); //:Gender
+            int genderOption;
+            try {
+                System.out.print("--> ");
+                genderOption = sc.nextInt();
+                sc.nextLine();
+            }catch (InputMismatchException e){
+                System.out.println("Error: Ingreso de datos Incorrecto!");
+                sc.nextLine();
+                continue;
+            }
+
+            switch (genderOption){
+                case 1:
+                    gender = Gender.M;
+                    break;
+                case 2:
+                    gender = Gender.F;
+                    break;
+                default:
+                    System.out.println("Error: No Selecciona una opción valida!");
+                    continue;
+            }
+            System.out.print("Peso: "); //:Double
+            try{
+                weight = sc.nextDouble();
+                sc.nextLine();
+            }catch (InputMismatchException e){
+                System.out.println("Error: Ingreso de datos Incorrecto!");
+                sc.nextLine();
+                continue;
+            }
+            break;
+        }while(true);
+
+        // Additional Data
+        switch (animalType){
+            case 1:
+                do {
+                    System.out.println("Raza: ");
+                    breed = sc.nextLine();
+                    if (breed.isBlank() || !breed.matches("^[a-zA-Z]+$")){
+                        System.out.println("Error: Formato de Raza Incorrecto!");
+                        continue;
+                    }
+                    break;
+                }while(true);
+                newAnimal = new Dog(name, age, owner, gender, weight, breed);
+                break;
+            case 2:
+                newAnimal = new Cat(name, age, owner, gender, weight);
+                break;
+            case 3:
+                int waterType = 0;
+                do {
+                    System.out.println("TIPO DE AGUA PESCADO");
+                    System.out.println("1) Fria");
+                    System.out.println("2) Tropical");
+                    System.out.print("--> ");
+                    try{
+                        waterType = sc.nextInt();
+                    }catch (InputMismatchException e){
+                        System.out.println("Error: Ingreso de datos Incorrecto!");
+                        sc.nextLine();
+                        continue;
+                    }
+                    switch (waterType){
+                        case 1:
+                            fishEnvironment = Environment.COLD;
+                            break;
+                        case 2:
+                            fishEnvironment = Environment.TROPICAL;
+                            break;
+                        default:
+                            System.out.println("Opcion invalida!");
+                            System.out.println("Porfavor, vuelva a ingresar...");
+                            continue;
+                    }
+                    newAnimal = new Fish(name, age, owner, gender, weight, fishEnvironment);
+                    break;
+                }while (true);
+                break;
+            case 4:
+                newAnimal = new Hamster(name, age, owner, gender, weight);
+                break;
+        }
+        animalList.add(newAnimal);
+    }
+}
